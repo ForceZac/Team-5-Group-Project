d6c4fc5848787836cc3df13ebbb609e0b6717aae
cj894884 <60268699+cj894884@users.noreply.github.com>
2021-05-02 22:04:02 -0400

    Create change_files.sh

---
+++ linpack/change_files.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+chmod +x /local/repository/linpack/bin/runme_xeon64
+chmod +x /local/repository/bin/linpack/liniput_xeon64
+chmod +x /local/repository/bin/linpack/xlinpack_xeon64
+chmod +x /local/repository/linpack/docker.sh
+chmod +x /local/repository/linpack/linux.sh
+chmod +x /local/repository/linpack/doit.sh
+chomd +x /local/repository/linpack/runBench.sh
