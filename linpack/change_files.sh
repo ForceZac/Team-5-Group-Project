4cb6e46c2a3f5f0204e29edf2220552df04a98fa
cj894884 <60268699+cj894884@users.noreply.github.com>
2021-05-02 22:05:25 -0400

    Update change_files.sh

--- linpack/change_files.sh
+++ linpack/change_files.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-
+#in order for this file to work it will also need to this command ran chmod +x change_files.sh
 chmod +x /local/repository/linpack/bin/runme_xeon64
 chmod +x /local/repository/bin/linpack/liniput_xeon64
 chmod +x /local/repository/bin/linpack/xlinpack_xeon64
 chmod +x /local/repository/linpack/docker.sh
 chmod +x /local/repository/linpack/linux.sh
 chmod +x /local/repository/linpack/doit.sh
-chomd +x /local/repository/linpack/runBench.sh
+chmod +x /local/repository/linpack/runBench.sh
