55cef7db63a9abb2ef7e281e00fdf514b2685c3d
cpopeck <70219803+cpopeck@users.noreply.github.com>
2021-05-05 12:07:54 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -23,9 +23,7 @@ link = request.LAN("lan")
 num_nodes = 3
 for i in range(num_nodes):
   if i == 0:
-    node = request.XenVM("arldcn28")
-    if i == 1:
-      node = request.XenVM("arldcn24")
+    node = request.XenVM("head")
   else:
     node = request.XenVM("worker-" + str(i))
   node.cores = 4
