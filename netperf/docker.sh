ad7f09ce7c8ceee550fd0ce809046d11c8d8056f
cpopeck <70219803+cpopeck@users.noreply.github.com>
2021-05-13 23:23:30 -0400

    Update docker.sh

--- netperf/docker.sh
+++ netperf/docker.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
+#will need to change where you ssh to based on your experiment
+ssh -p 22 cpop@pcvm757-2.emulab.net sudo apt-get update
+ssh -p 22 cpop@pcvm757-2.emulab.net sudo apt-get install -y netperf
+ssh -p 22 cpop@pcvm757-1.emulab.net git clone https://github.com/cj894884/Team-5-Group-Project
+ssh -p 22 cpop@pcvm757-1.emulab.net "cd Team*/netperf && docker build -t netserver ."
 
-# server and client are reversed compared to nuttcp
-SERVER=arldcn24
-CLIENT=arldcn28
-DIR=`pwd`
-
-# build the container (assumes the spyre git repo is in NFS)
-cp /usr/bin/netserver .
-ssh $CLIENT docker build -t netserver $DIR
-
-sudo service netperf stop
-ssh $SERVER docker run -d -p 12865:12865 netserver
-
-ssh $CLIENT netperf -l 60 -H 10.71.0.24 -t TCP_RR -- -r 100,200
-ssh $CLIENT netperf -l 60 -H 10.71.0.24 -t UDP_RR -- -r 100,200
-
+ssh -p 22 cpop@pcvm757-1.emulab.net docker run -d --network="host" netserver
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+do
+ssh -p 22 cpop@pcvm757-2.emulab.net netperf -l 60 -H 10.71.0.24 -t TCP_RR -- -r 100,200
+done
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+do
+ssh -p 22 cpop@pcvm757-2.emulab.net netperf -l 60 -H 10.71.0.24 -t UDP_RR -- -r 100,200
+done
 # clean up
 #ssh $SERVER docker stop netserver
 
