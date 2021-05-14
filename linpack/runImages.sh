a7c82347bbff3af5ad3a66ce14dcdc1cd86e60e1
cj894884 <60268699+cj894884@users.noreply.github.com>
2021-05-14 16:36:43 -0400

    Create runImages.sh

---
+++ linpack/runImages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker pull cj894884/dockerhub:linpackonesocket
+docker run -ti --priveleged cj894884/dockerhub:linpackonesocket
+docker image rm -f cj894884/dockerhub:linpackonesocket
+
+docker pull cj894884/dockerhub:linpacktwosocket
+docker run -ti --pivelged cj894884/dockerhub:linpacktwosocket
+docker image rm -f cj894884/dockerhub:linpacktwosocket
+
+cd bin
+time numactl --physcpubind=0-7,16-23 --localloc ./runme_xeon64
+time numactl --physcpubind=0-31 --interleave=0,1 ./runme_xeon64
